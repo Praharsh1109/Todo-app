4fd096e793a062e7e99226f4ad1b50150bbaec02
Praharsh1109 <ppraharsh11@gmail.com>
2024-01-29 21:12:16 -0500

    click add

--- Frontend/component/CreateTodo.jsx
+++ Frontend/component/CreateTodo.jsx
@@ -1,7 +1,14 @@
-export function CreateTodo(){
-    return <div>
-        <input style={{padding:20 ,margin:20}} type="text" placeholder="title"/><br />
-        <input style={{padding:20 ,margin:20}} type="text" placeholder="Description"/><br />
-        <button>add a todo</button>
-    </div>
-}
+export function CreateTodo() {
+    const handleClick = () => {
+      alert("You clicked");
+    };
+  
+    return (
+      <div>
+        <input style={{ padding: 10, margin: 10 }} type="text" placeholder="Title" /><br />
+        <input style={{ padding: 10, margin: 10 }} type="text" placeholder="Description" /><br /><br />
+        <button style={{ padding: 10, margin: 10 }} onClick={handleClick}>Add a todo</button>
+      </div>
+    );
+  }
+  
