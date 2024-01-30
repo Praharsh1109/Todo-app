79a9ba6b707d5046bf31937c05272d965e2890f9
Praharsh1109 <ppraharsh11@gmail.com>
2024-01-29 20:31:46 -0500

    frontend Added

---
+++ Frontend/component/CreateTodo.jsx
@@ -0,0 +1,7 @@
+export function CreateTodo(){
+    return <div>
+        <input style={{padding:20 ,margin:20}} type="text" placeholder="title"/><br />
+        <input style={{padding:20 ,margin:20}} type="text" placeholder="Description"/><br />
+        <button>add a todo</button>
+    </div>
+}
