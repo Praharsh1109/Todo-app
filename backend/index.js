8e305ab034b747504649740022ba992842c9d471
Praharsh Patel <53941184+Praharsh1109@users.noreply.github.com>
2024-02-05 19:50:38 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,14 @@ const { todo } = require("./db");
 const cors = require("cors");
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
 
 app.post("/todo", async function(req, res) {
     const createPayload = req.body;
